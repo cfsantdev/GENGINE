b9c609212046b2a10b91f25acf16d01d204eabb0
cfsantdev <cfsant4gamers@gmail.com>
2026-03-04 08:56:10 -0300

    Update gltf-component.js
    Fix:
    import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.118.1/build/three.module.js';

--- JS/gltf-component.js
+++ JS/gltf-component.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.118.1/build/three.module.js';
 
 import {GLTFLoader} from 'https://cdn.jsdelivr.net/npm/three@0.118.1/examples/jsm/loaders/GLTFLoader.js';
 import {FBXLoader} from 'https://cdn.jsdelivr.net/npm/three@0.118.1/examples/jsm/loaders/FBXLoader.js';
